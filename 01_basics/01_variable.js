1d6156b9627c8e473b2cf54d0dd05d70aa19b6cd
Nilesh Kumar Singh <113060638+nileshsingh26@users.noreply.github.com>
2024-04-27 17:34:22 +0000

    container config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,10 @@
+const accountId="nilesh_26"
+let accountEmail="satyamsinghrock2002@hmail"
+var accountPassword="123456"
+accountCity="Lucnkow"
+let accountState
+console.log(accountId);
+/*
+never use var use let and const only bz issue of block or scope
+*/
+console.table([accountId,accountEmail,accountPassword])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all js code as newer version
+
+// we are using nodejs not browser
+
+/*Data types on js:
+1.number
+2.bigint
+3.string
+4.boolean
+5.null-->null is an object
+6.undefined 
+
+*/
+
+console.log(typeof('strinsds'))
