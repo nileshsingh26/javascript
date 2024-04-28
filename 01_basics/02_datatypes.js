fbb524dcd1d88b759a5ac47992294a0fde43c317
Nilesh Kumar Singh <113060638+nileshsingh26@users.noreply.github.com>
2024-04-28 19:34:20 +0000

    maths

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,15 +1,57 @@
-"use strict"; //treat all js code as newer version
+// "use strict"; //treat all js code as newer version
 
-// we are using nodejs not browser
+// // we are using nodejs not browser
 
-/*Data types on js:
-1.number
-2.bigint
-3.string
-4.boolean
-5.null-->null is an object
-6.undefined 
+// /*Data types on js:
+// 1.number
+// 2.bigint
+// 3.string
+// 4.boolean
+// 5.null-->null is an object
+// 6.undefined 
 
-*/
+// */
 
-console.log(typeof('strinsds'))
+// console.log(typeof('strinsds'))
+// let myName="Nilesh"
+// let anotherName= myName
+// anotherName="chaiaurcode"
+// console.log(myName)
+// console.log(anotherName)
+
+
+// let user={
+//     email:"xyz@gmail.com", 
+//     typo:'dsfs@hds'
+// }
+
+// let userTwo = user
+// userTwo.email="niles@gmail.com"
+
+
+// // the object make changes to the refernce that is to the origibal values
+
+// console.log(user.email);
+// console.log(userTwo.email)
+
+// let name = "Nilesh Kumar Singh"
+// let classes = "CS_IOT_3A"
+// console.log(name)
+
+// let arr1=["Nilesh","Satyam","Anshika","Pooja","vishnu"]
+// console.log(arr1[1])
+// console.table([])
+
+const newName="   Nilesh  "
+const oldName= newName
+//substring buildin function
+console.log(oldName.substring(3,6))
+//trim function 
+console.log(oldName.slice(3,6))
+//replace function
+const email="satyamsingh@gmail.com"
+console.log(email.replace("satyam","nilesh")) 
+console.log(email.includes("nilesh"))  
+
+let strings= "hello my name is nilesh"
+console.log(strings.split(" "))
---
+++ 01_basics/03_mat.js
@@ -0,0 +1,24 @@
+const score=100
+console.log(score)
+
+const balance= new Number(2333)
+console.log(balance)
+
+// o/p:
+// 100
+// [Number: 2333]
+
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+
+// o/p:
+// 4
+// 2333.00
+
+
+// **************************************maths****************************************
+const min=1
+const max=6
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+ 
